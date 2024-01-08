130b1611e1865ce06981069d829cf8f24584f170
MurodillaErgashev03 <ergashevmurodilla27@gmail.com>
2024-01-08 11:08:57 +0500

    finish

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let money = prompt("Sizda qancha pul bor")
+let  ticket = 500;
+let hotel = 250;
+let museum = 120;
+
+let dollarSum = 10650.34 * (ticket + hotel);
+let yevroSum = 10650.03 * museum;
+
+let cost = dollarSum + yevroSum;
+
+
+
+console.log("Harajatlaringiz " + cost + " som", "sizda " + money + " som pul bor"  ) 
